be7a7f62b38e3100dd6673f1c2175b309f4545ac
EC2 Default User <ec2-user@ip-172-31-45-122.ap-northeast-1.compute.internal>
2023-11-07 12:39:15 +0000

    fix4

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -18,7 +18,7 @@ class Public::BooksController < ApplicationController
     genre_names = params[:genre_name]
     @book = Book.new(book_params)
     #@book.score = Language.get_data(book_params[:explanation])
-    if @book.save!
+    if @book.save
       genre_names.each do |genre_name|
         if genre_name.present?&&genre_name != ""
           genre = Genre.find_or_create_by(user_id: current_user, name: genre_name)
